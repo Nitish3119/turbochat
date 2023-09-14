1803fd9180df68cfd3ecae840a115ad8141e4a12
Ongraph <nitish.agarwal@ongraph.com>
2023-09-14 18:45:22 +0530

    create new room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,10 +4,12 @@ class RoomsController < ApplicationController
   def index
     @room = Room.new
     @rooms = Room.public_rooms
+    @single_room = Room.find_by(id: params[:id])
     @users = User.all_except(current_user)
   end
 
   def create
     @room = Room.create(name: params["room"]["name"])
+    redirect_to rooms_path
   end
 end
